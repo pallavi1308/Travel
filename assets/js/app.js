977c4b951fff685740d0893cbba87f8097f97557
pallavi1308 <87478469+pallavi1308@users.noreply.github.com>
2022-01-19 13:53:44 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,86 @@
+
+      
+var https = require("https");
+var path = require("path");
+var fs = require("fs");
+
+// The authentication key (API Key).
+// Get your own by registering at https://app.pdf.co/documentation/api
+const API_KEY = "***********************************";
+
+// Direct URL of source PDF file.
+const SourceFileUrl = "https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-edit/sample.pdf";
+
+// Comma-separated list of page indices (or ranges) to process. Leave empty for all pages. Example: '0,2-5,7-'.
+const Pages = "";
+
+// PDF document password. Leave empty for unprotected documents.
+const Password = "";
+
+// Destination PDF file name
+const DestinationFile = "./result.pdf";
+
+// Image params
+const Type = "image";
+const X = 400;
+const Y = 20;
+const Width = 119;
+const Height = 32;
+const ImageUrl = "https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-edit/logo.png";
+
+// * Add image *
+// Prepare request to `PDF Edit` API endpoint
+var queryPath = `/v1/pdf/edit/add`;
+
+// JSON payload for api request
+var jsonPayload = JSON.stringify({
+    name: path.basename(DestinationFile),
+    password: Password,
+    pages: Pages,
+    url: SourceFileUrl,
+    type: Type,
+    x: X,
+    y: Y,
+    width: Width,
+    height: Height,
+    urlimage: ImageUrl
+});
+
+var reqOptions = {
+    host: "api.pdf.co",
+    method: "POST",
+    path: queryPath,
+    headers: {
+        "x-api-key": API_KEY,
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(jsonPayload, 'utf8')
+    }
+};
+// Send request
+var postRequest = https.request(reqOptions, (response) => {
+    response.on("data", (d) => {
+        // Parse JSON response
+        var data = JSON.parse(d);
+
+        if (data.error == false) {
+            // Download the PDF file
+            var file = fs.createWriteStream(DestinationFile);
+            https.get(data.url, (response2) => {
+                response2.pipe(file).on("close", () => {
+                    console.log(`Generated PDF file saved to '${DestinationFile}' file.`);
+                });
+            });
+        }
+        else {
+            // Service reported error
+            console.log(data.message);
+        }
+    });
+}).on("error", (e) => {
+    // Request error
+    console.error(e);
+});
+
+// Write request data
+postRequest.write(jsonPayload);
+postRequest.end();
